030bd2ddea9e647ca1b0387cb6a4dde24524f8ae
Sarbu Iulia Iustina <48455236+iustina02@users.noreply.github.com>
2020-04-01 15:45:09 +0300

    Laboratorul 7

---
+++ Proiect2/ObjectWCF/InterfaceWCF.cs
@@ -0,0 +1,13 @@
+﻿using ModelMyPhotos;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace ObjectWCF
+{
+    [ServiceContract]
+    interface InterfacePhoto
+    {
+        [OperationContract]
+        void AddPhoto(Photos newPhoto);
+    }
+}
