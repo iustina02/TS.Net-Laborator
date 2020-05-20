e3478e0b04db0cbafcf2d31577f4d9e40b9eb5e8
Sarbu Iulia Iustina <48455236+iustina02@users.noreply.github.com>
2020-05-20 16:11:31 +0300

    Laboratorul 11 Proiectul 3

---
+++ Proiect3/RazorPagesMyPhotos/Models/FilmsDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace RazorPagesMyPhotos.Models
+{
+  
+    [DataContract(IsReference = true)]
+    public partial class FilmsDTO
+    {
+        public FilmsDTO()
+        {
+            this.Persons = new HashSet<PersonsDTO>();
+            this.Places_Photo = new HashSet<PlacesDTO>();
+        }
+
+        [DataMember]
+        public int Film_Id { get; set; }
+        [DataMember]
+        public Nullable<System.DateTime> Date { get; set; }
+        [DataMember]
+        public string Event { get; set; }
+        [DataMember]
+        public string Landscape { get; set; }
+        [DataMember]
+        public string Path { get; set; }
+
+        [DataMember]
+        public virtual ICollection<PersonsDTO> Persons { get; set; }
+        [DataMember]
+        public virtual ICollection<PlacesDTO> Places_Photo { get; set; }
+    }
+}
---
+++ Proiect3/RazorPagesMyPhotos/Models/PhotosDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace RazorPagesMyPhotos.Models
+{
+    [DataContract(IsReference = true)]
+    public partial class PhotosDTO
+    {
+        public PhotosDTO()
+        {
+            this.Persons_Photo = new HashSet<PersonsDTO>();
+        }
+
+        [DataMember]
+        public int Photo_Id { get; set; }
+        [DataMember]
+        public Nullable<System.DateTime> Date { get; set; }
+        [DataMember]
+        public string Event { get; set; }
+        [DataMember]
+        public string Landscape { get; set; }
+        [DataMember]
+        public string Path { get; set; }
+
+        [DataMember]
+        public virtual PlacesDTO Places_Photo { get; set; }
+        [DataMember]
+        public virtual ICollection<PersonsDTO> Persons_Photo { get; set; }
+    }
+}
